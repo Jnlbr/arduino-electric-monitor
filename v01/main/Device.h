12f7499bc85254fa478cb82c9dbfb729e192eea6
Jean Lambert <jeeaan1702@gmail.com>
2018-11-20 06:47:01 -0800

    libraries and impl

--- v01/main/Device.h
+++ v01/main/Device.h
@@ -1,23 +1,27 @@
 #ifndef Device_h
 #define Device_h
 
-#include "Arduino.h"
+#include <Arduino.h>
 
 class Device {
   public:
-    // Device();
+    Device();
+    Device(int id, String code, String name, bool status, int volt);
+    void setId(int id);
+    int getId();
     void setCode(String code);
     String getCode();
     void setName(String name);
     String getName();
-    void setDeviceType(int type);
-    int getDeviceType();
     void setStatus(bool status);
     bool getStatus();
+    void setVoltage(int volt);
+    int getVoltage();
   private:
+    int _volt;
+    int _id;
     String _code;
     String _name;
-    int _deviceType;
     bool _status;
 };
 
---
+++ v01/main/readVoltage.ino
@@ -0,0 +1,17 @@
+float readVoltage(int pin) { 
+  unsigned int total = 0;
+  
+  for (int i=0; i<AVG_NUM; i++) {                                               
+    total += analogRead(pin);     // read the input pin
+    Serial.println(analogRead(pin));
+    // The delay is arbitrary.
+    // It can be 0 to 20ms.
+    // Test for your own circuit what is best.
+    delayMicroseconds(50); // 50;
+  }
+  // The voltage is RAW_ADC / 1023 * 5V and also divided by the amount of samples for average.
+  // Convert every integer to float before calculating something.
+  // This way, the extra bits gained by averaging are used in the calculation.
+  float voltage = (float) total / 1023.0 * 5.0 / (float) AVG_NUM;
+  return voltage;
+}
